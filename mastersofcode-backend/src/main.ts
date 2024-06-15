96eab06d65f34f92ae7046acad7312b15bca8b53
Gabriel Santino <ggs2209@gmail.com>
2024-06-15 20:44:58 -0300

    Safe Update Working

--- mastersofcode-backend/src/main.ts
+++ mastersofcode-backend/src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+const cors = require('cors');
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.use(cors());
   await app.listen(3002);
+  
+  
 }
+
+
 bootstrap();
